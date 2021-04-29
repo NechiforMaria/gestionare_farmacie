927235c11c6580d69052fe353acbce5edaa83381
NechiforMaria <bianca.nechifor@student.usv.ro>
2021-04-29 11:40:58 +0300

    update

---
+++ lab 1/medicament.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace lab_1
+{
+    class medicament
+    {
+        public string Nume { get; set; }
+        public int Nr_capsule { get; set; }
+        public string Valabilitate { get; set; }
+        public medicament(string nume,
+            int nr_capsule,
+            string valabilitate)
+        {
+            Nume = nume;
+            Nr_capsule = nr_capsule;
+            Valabilitate = valabilitate;
+        }
+    }
+}
