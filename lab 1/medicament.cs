4ffbcd5fe50ced98fffc5748343dc39e8d0b5894
NechiforMaria <bianca.nechifor@student.usv.ro>
2021-05-03 19:09:35 +0300

    update

--- lab 1/medicament.cs
+++ lab 1/medicament.cs
@@ -33,34 +33,9 @@ namespace gestionarea_farmaciei
             return $"Medicamentul {Nume} are {Nr_capsule} capsule si expira la data de {Valabilitate}";
         }
 
-        private const string SEPARATOR_AFISARE = " ";
-        private const char SEPARATOR_PRINCIPAL_FISIER = ';';
-        public string ConversieLaSir_PentruScriereInFisier()
-        {
-            string mValabilitate = string.Empty;
-            if (Valabilitate != null)
-            {
-                mValabilitate = string.Join(SEPARATOR_AFISARE, Valabilitate);
-            }
-            string m = string.Format("{1}{0}{2}{0}{3}{0}{4}",
-                SEPARATOR_PRINCIPAL_FISIER, Nume.ToString(), (Nume ?? " NECUNOSCUT "), (Valabilitate ?? " NECUNOSCUT "), mValabilitate);
+       
 
-            return m;
-        }
-        private const int NUME = 0;
-        private const int NR_CAPSULE = 1;
-        private const int VALABILITATE = 2;
-
-        public Medicament(string linieFisier)
-        {
-            var dateFisier = linieFisier.Split(SEPARATOR_PRINCIPAL_FISIER);
-            
-            //ordinea de preluare a campurilor este data de ordinea in care au fost scrise in fisier prin apelul implicit al metodei ToString()
-            Nume = dateFisier[NUME];
-            Nr_capsule = NR_CAPSULE;
-            Valabilitate= dateFisier[VALABILITATE];
-            
-        }
+       
     }
 }
 
