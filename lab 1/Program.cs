8193956f030c4d9b03d1354589eda54b7f9876af
NechiforMaria <bianca.nechifor@student.usv.ro>
2021-05-03 19:06:50 +0300

    update

--- lab 1/Program.cs
+++ lab 1/Program.cs
@@ -1,13 +1,52 @@
 ﻿using System;
+using System.Collections;
 
-namespace lab_1
+namespace gestionarea_farmaciei
 {
     class Program
     {
+
+        private const int NR_MINIM_MEDICAMENTE = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nrMedicamente;
+            do
+            {
+                Console.WriteLine("Introduceti numarul de medicamente");
+                Int32.TryParse(Console.ReadLine(), out nrMedicamente);
+            } while (nrMedicamente <= NR_MINIM_MEDICAMENTE);
+
+            Medicament[] medicamente = new Medicament[nrMedicamente];
+
+            for (int i = 0; i < nrMedicamente; i++)
+            {
+                Console.WriteLine($"Medicament {i + 1}:");
+                Console.Write($"Introduceti denumirea medicamentului ");
+                string nume = Console.ReadLine();
+                Console.Write($"Introduceti numarul de capsule ");
+                int nrCapsule = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"Introduceti valabilitatea medicamentului(zz.ll.aaaa) ");
+                string valabilitate = Console.ReadLine();
+                Medicament medicamentCitit = new Medicament(nume, nrCapsule, valabilitate);
+                medicamente[i] = medicamentCitit;
+
+            }
+
+            Console.WriteLine("\nInformatiile despre medicamente sunt:");
+
+            for (int i = 0; i < nrMedicamente; i++)
+            {
+                Console.WriteLine(medicamente[i].ConversieLaSir());
+                
+               
+            }
+
             Console.ReadKey();
         }
     }
-}
+
+
+       
+        
+    }
+
--- lab 1/medicament.cs
+++ lab 1/medicament.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace lab_1
+namespace gestionarea_farmaciei
 {
-    class medicament
+    class Medicament
     {
         public string Nume { get; set; }
         public int Nr_capsule { get; set; }
         public string Valabilitate { get; set; }
-        public medicament(string nume,
+
+        //constructor fara parametri
+        public Medicament()
+        {
+            Nume = string.Empty;
+            Nr_capsule = 0;
+            Valabilitate = string.Empty;
+        }
+        //constructor cu parametri
+        public Medicament(string nume,
             int nr_capsule,
             string valabilitate)
         {
@@ -18,5 +27,41 @@ namespace lab_1
             Nr_capsule = nr_capsule;
             Valabilitate = valabilitate;
         }
+
+        public string ConversieLaSir()
+        {
+            return $"Medicamentul {Nume} are {Nr_capsule} capsule si expira la data de {Valabilitate}";
+        }
+
+        private const string SEPARATOR_AFISARE = " ";
+        private const char SEPARATOR_PRINCIPAL_FISIER = ';';
+        public string ConversieLaSir_PentruScriereInFisier()
+        {
+            string mValabilitate = string.Empty;
+            if (Valabilitate != null)
+            {
+                mValabilitate = string.Join(SEPARATOR_AFISARE, Valabilitate);
+            }
+            string m = string.Format("{1}{0}{2}{0}{3}{0}{4}",
+                SEPARATOR_PRINCIPAL_FISIER, Nume.ToString(), (Nume ?? " NECUNOSCUT "), (Valabilitate ?? " NECUNOSCUT "), mValabilitate);
+
+            return m;
+        }
+        private const int NUME = 0;
+        private const int NR_CAPSULE = 1;
+        private const int VALABILITATE = 2;
+
+        public Medicament(string linieFisier)
+        {
+            var dateFisier = linieFisier.Split(SEPARATOR_PRINCIPAL_FISIER);
+            
+            //ordinea de preluare a campurilor este data de ordinea in care au fost scrise in fisier prin apelul implicit al metodei ToString()
+            Nume = dateFisier[NUME];
+            Nr_capsule = NR_CAPSULE;
+            Valabilitate= dateFisier[VALABILITATE];
+            
+        }
     }
 }
+
+
